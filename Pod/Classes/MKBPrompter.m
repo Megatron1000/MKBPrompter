90c1137b6a5956152e6df24f32c512709bd142fd
Mark Bridges <markbridges9@gmail.com>
2016-05-30 21:07:57 +0100

    fixed failure to build

--- Pod/Classes/MKBPrompter.m
+++ Pod/Classes/MKBPrompter.m
@@ -13,8 +13,6 @@ static NSString *mkbKeyRunCount = @"runCount";
 static NSString *mkbKeyStopRatePrompting = @"stopRate";
 static NSString *mkbKeyStopOtherAppPrompting = @"stopOtherApps";
 
-"https://itunes.apple.com/us/app/id1035425291?mt=8"
-
 static NSString *reviewLinkFormat = @"https://itunes.apple.com/us/app/id%@?mt=8";
 static NSString *companyLinkFormat = @"https://itunes.apple.com/us/developer/id%@";
 
